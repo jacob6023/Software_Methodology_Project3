8f88d9470643c7be18fc3af2458f5c98ddaad014
Jack Crosby <jackcrosbystack@gmail.com>
2024-09-13 10:20:50 -0400

    testing commit

---
+++ testClass.java
@@ -0,0 +1,10 @@
+/**
+ * This class is just to test if our uploads to github are succesful.
+ */
+package Project_1;
+public class testClass{
+
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
