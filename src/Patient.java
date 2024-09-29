67c900f97f6a13ada6c7ac486436bd777f88ffac
VikramKad <vikkadyan18@gmail.com>
2024-09-29 12:36:37 -0400

    t

--- src/Patient.java
+++ src/Patient.java
@@ -3,5 +3,41 @@ package Project_1.src;
 public class Patient implements Comparable<Patient>{
     private Profile profile;
     private Visit visits; //a LL of visits (completed appt. )
-    public int charge(){} //traverse the LL to compute the charge
+
+    //constructor for Patients
+    public Patient(Profile profile){
+        this.profile = profile;
+        this.visits = null;
+    }
+
+    //override for equals, toString, and compareTo
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Patient diff = (Patient) obj;
+        return profile.equals(diff.profile);
+    }
+    @Override
+    public String toString() {
+        //later
+    }
+
+    @Override
+    public int compareTo(Patient other) {
+        //later
+    }
+
+    //traverse linked list to find the total charge
+    public int charge(){
+        int total = 0;
+        Visist currentVisit=visits;
+        while (currentVisit!=null){
+            total += currentVisit.getAppointment().getProvider().getSpecialty().getCharge();
+            currentVisit = currentVisit.getNext();
+        }
+        return total;
+    } //traverse the LL to compute the charge
+
 }
+
